64b62612832b0fa17c0583229e709bb07e95cc49
Zack Martin <zackmartin238@gmail.com>
2024-04-10 19:55:15 -0500

    committed

--- D and D Class maker/editExistingCharacter.py
+++ D and D Class maker/editExistingCharacter.py
@@ -5,22 +5,35 @@ def quit_program():
 def select_file():
     path = os.path.dirname(os.path.realpath(__file__)),
     global currentFile
+    global fileSelected
     currentFile = tkinter.filedialog.askopenfilename(
         parent=mainWindow,
         initialdir= str(path),
         title='Choose file',
         filetypes=[('PDF files', '*.pdf')],
     )
-    
+
     viewCharacter()
-    
+
+def print_using_printer():
+    if os.name=="nt":
+        os.startfile(currentFile)
+    if os.name != "nt":
+        lpr =  subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
+        lpr.stdin.write(currentFile)
+
+
 def viewCharacter():
     webbrowser.open_new(currentFile)
+    printButton = Button(topframe, text="Print file to Printer", command=print_using_printer)
+    printButton.pack(in_=topframe, side=LEFT)
+
 if __name__ == "__main__":
     from tkinter import *
     import tkinter.filedialog
     import os
     import webbrowser
+    import subprocess
     global currentFile
     mainWindow = Tk()
     mainWindow.title("Edit Existing Character")
@@ -42,5 +55,6 @@ if __name__ == "__main__":
 
     FileSelectButton = Button(topframe, text="Select File to edit", command=select_file)
     FileSelectButton.pack(in_=topframe, side=LEFT)
+    
 
     mainloop()
